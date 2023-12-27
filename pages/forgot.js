69eca41b6ff291bbacd320a0d3590f3c414699a7
Prince Kumar <princesd991@gmail.com>
2023-12-27 18:50:55 +0530

    🫙Adding MongoDB

--- pages/forgot.js
+++ pages/forgot.js
@@ -53,19 +53,9 @@ const forgot = () => {
               </button>
             </div>
           </form>
-
-          <p className="mt-10 text-center text-sm text-gray-500">
-            Not a member?{" "}
-            <a
-              href="#"
-              className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
-            >
-              Start a 14 day free trial
-            </a>
-          </p>
         </div>
       </div>
   )
 }
 
-export default forgot
+export default forgot 
