f8c8daae6c44bfdfa9f4b969539aed516a9092f2
Prince Kumar <princesd991@gmail.com>
2023-12-30 00:53:18 +0530

    🔐 implement Login

--- pages/login.js
+++ pages/login.js
@@ -1,102 +1,303 @@
-import React from "react";
-import logo from "../images/codechic logo.png";
+import React, { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useRouter } from "next/router";
 
-const login = () => {
+const Login = () => {
+  const router = useRouter();
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const handleChange = (e) => {
+    if (e.target.name == "name") {
+      setName(e.target.value);
+    } else if (e.target.name == "email") {
+      setEmail(e.target.value);
+    } else if (e.target.name == "password") {
+      setPassword(e.target.value);
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const data = { name, email, password };
+
+    let res = await fetch(`http://localhost:3000/api/login`, {
+      method: "POST", // or 'PUT'
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    let response = await res.json();
+    console.log(response);
+
+    setName("");
+    setEmail("");
+    setPassword("");
+    if (response.success) {
+      localStorage.setItem("token", response.token);
+      toast.success("Successfully Login!", {
+        position: "top-center",
+        autoClose: 1500,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+      
+      setTimeout(()=>{
+        router.push("/");
+      },1000);
+    } else {
+      toast.error(response.error, {
+        position: "top-center",
+        autoClose: 1500,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+    }
+  };
+
   return (
-    <>
-      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img
-            className="mx-auto h-10 w-auto"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-            alt="Logo"
-          />
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Sign in to your account
-          </h2>
-          <p className="mt-2 text-center text-sm text-gray-600">
-            Or{" "}
-            <a
-              href="/signup"
-              className="font-medium text-pink-600 hover:text-pink-500"
-            >
-              SignUp
-            </a>
-          </p>
-        </div>
-
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form className="space-y-6" action="#" method="POST">
-            <div>
-              <label
-                htmlFor="email"
-                className="block text-sm font-medium leading-6 text-gray-900"
+    <div>
+      <ToastContainer
+        position="top-center"
+        autoClose={1500}
+        limit={5}
+        hideProgressBar
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colors"
+      />
+      <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <div className="w-full max-w-md space-y-8">
+          <div>
+            <Link href={"/"}>
+              <img
+                className="mx-auto h-10 w-auto"
+                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                alt="Logo"
+              />
+              {/* <Image
+              className="mx-auto"
+              src={Company}
+              alt="Company"
+              width={200}
+               height={40}
+            /> */}
+            </Link>
+            <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+              Sign in to your account
+            </h2>
+            <p className="mt-2 text-center text-sm text-gray-600">
+              Or
+              <Link
+                href={"/signup"}
+                className="font-medium text-pink-600 hover:text-pink-500 mx-2"
               >
-                Email address
-              </label>
-              <div className="mt-2">
+                Sign Up
+              </Link>
+            </p>
+          </div>
+          <form
+            onSubmit={handleSubmit}
+            className="mt-8 space-y-6"
+            action="#"
+            method="POST"
+          >
+            <input type="hidden" name="remember" value="true" />
+            <div className="space-y-px rounded-md shadow-sm">
+              <div className="mb-4">
+                <label htmlFor="email" className="sr-only">
+                  Email address
+                </label>
                 <input
+                  onChange={handleChange}
+                  value={email}
                   id="email"
                   name="email"
                   type="email"
                   autoComplete="email"
                   required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-pink-500 focus:outline-none focus:ring-pink-500 sm:text-sm"
+                  placeholder="Email address"
                 />
               </div>
-            </div>
-            <div>
-              <div className="flex items-center justify-between">
-                <label
-                  htmlFor="password"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
+              <div>
+                <label htmlFor="password" className="sr-only">
                   Password
                 </label>
-                <div className="text-sm">
-                  <a
-                    href="/forgot"
-                    className="font-semibold text-indigo-600 hover:text-indigo-500"
-                  >
-                    Forgot password?
-                  </a>
-                </div>
-              </div>
-              <div className="mt-2">
                 <input
+                  onChange={handleChange}
+                  value={password}
                   id="password"
                   name="password"
                   type="password"
                   autoComplete="current-password"
                   required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-pink-500 focus:outline-none focus:ring-pink-500 sm:text-sm"
+                  placeholder="Password"
                 />
+              </div>
+            </div>
+
+            <div className="flex items-center justify-between">
+              <div className="flex items-center">
+                <input
+                  onChange={handleChange}
+                  id="remember-me"
+                  name="remember-me"
+                  type="checkbox"
+                  className="h-4 w-4 rounded border-gray-300 text-pink-600 focus:ring-pink-500"
+                />
+                <label
+                  htmlFor="remember-me"
+                  className="ml-2 block text-sm text-gray-900"
+                >
+                  Remember me
+                </label>
+              </div>
+
+              <div className="text-sm">
+                <Link
+                  href="/forgot"
+                  className="font-medium text-pink-600 hover:text-pink-500"
+                >
+                  Forgot your password?
+                </Link>
               </div>
             </div>
 
             <div>
               <button
                 type="submit"
-                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                className="group relative flex w-full justify-center rounded-md border border-transparent bg-pink-600 py-2 px-4 text-sm font-medium text-white hover:bg-pink-700 focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-offset-2"
               >
                 Sign in
               </button>
             </div>
           </form>
-
-          <p className="mt-10 text-center text-sm text-gray-500">
-            Not a member?{" "}
-            <a
-              href="#"
-              className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
-            >
-              Start a 14 day free trial
-            </a>
-          </p>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
-export default login;
+export default Login;
+
+// import React from "react";
+// import logo from "../images/codechic logo.png";
+
+// const login = () => {
+//   return (
+//     <>
+//       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+//         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+//           <img
+//             className="mx-auto h-10 w-auto"
+//             src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+//             alt="Logo"
+//           />
+//           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+//             Sign in to your account
+//           </h2>
+//           <p className="mt-2 text-center text-sm text-gray-600">
+//             Or{" "}
+//             <a
+//               href="/signup"
+//               className="font-medium text-pink-600 hover:text-pink-500"
+//             >
+//               SignUp
+//             </a>
+//           </p>
+//         </div>
+
+//         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+//           <form className="space-y-6" action="#" method="POST">
+//             <div>
+//               <label
+//                 htmlFor="email"
+//                 className="block text-sm font-medium leading-6 text-gray-900"
+//               >
+//                 Email address
+//               </label>
+//               <div className="mt-2">
+//                 <input
+//                   id="email"
+//                   name="email"
+//                   type="email"
+//                   autoComplete="email"
+//                   required
+//                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+//                 />
+//               </div>
+//             </div>
+//             <div>
+//               <div className="flex items-center justify-between">
+//                 <label
+//                   htmlFor="password"
+//                   className="block text-sm font-medium leading-6 text-gray-900"
+//                 >
+//                   Password
+//                 </label>
+//                 <div className="text-sm">
+//                   <a
+//                     href="/forgot"
+//                     className="font-semibold text-indigo-600 hover:text-indigo-500"
+//                   >
+//                     Forgot password?
+//                   </a>
+//                 </div>
+//               </div>
+//               <div className="mt-2">
+//                 <input
+//                   id="password"
+//                   name="password"
+//                   type="password"
+//                   autoComplete="current-password"
+//                   required
+//                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+//                 />
+//               </div>
+//             </div>
+
+//             <div>
+//               <button
+//                 type="submit"
+//                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+//               >
+//                 Sign in
+//               </button>
+//             </div>
+//           </form>
+
+//           <p className="mt-10 text-center text-sm text-gray-500">
+//             Not a member?{" "}
+//             <a
+//               href="#"
+//               className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+//             >
+//               Start a 14 day free trial
+//             </a>
+//           </p>
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default login;
--- pages/signup.js
+++ pages/signup.js
@@ -98,7 +98,6 @@ const Signup = () => {
           <form
             onSubmit={handleSubmit}
             className="mt-8 space-y-6"
-            action="#"
             method="POST"
           >
             {/* <input type="hidden" name="remember" value="true" /> */}
